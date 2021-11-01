8bbf5dc573f924981e61a4a68a17c471d51946ec
KalenchitsKarina <kkalenchits@yandex.by>
2021-11-01 12:59:09 +0300

    found the maximum element

--- task_6.py
+++ task_6.py
@@ -1,8 +1,7 @@
 from random import randint
 
-#a=int(input('Enter the lower limit of the matrix range : '))
-#b=int(input('Enter the upper limit of the matrix range : '))
-a,b=0,10
+a=int(input('Enter the lower limit of the matrix range : '))
+b=int(input('Enter the upper limit of the matrix range : '))
 n=int(input('Enter n (size of matrix) : '))
 m=int(input('Enter m (size of matrix) : '))
 
@@ -10,12 +9,18 @@ matrix = []
 
 for i in range (n):
     matrix.append([])
-print (matrix)
-
+    
 for i in range(n):
     for j in range (m):
         matrix[i].append(randint(a,b))
 
 for i in matrix:
     print (i)
+
+max=0
+for i in range (n):
+    for j in range (m):
+        if matrix[i][j]>max:
+            max=matrix[i][j]
+print ('maximum matrix element : ',max )
 
