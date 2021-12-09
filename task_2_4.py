7f91e89da76115ea69b4363d1938f52b732220ef
KalenchitsKarina <kkalenchits@yandex.by>
2021-12-09 10:54:57 +0300

    update

--- task_2_4.py
+++ task_2_4.py
@@ -1,6 +1,6 @@
-a=input('Enter the string: ')
-if len(a)>=2:
-    b=a[:-2:]
+a = input('Enter the string: ')
+if len(a) >= 2:
+    b = a[:-2:]
     print(b)
 else:
     print ('There are less than 2 characters in the string')
--- task_2_5.py
+++ task_2_5.py
@@ -1,6 +1,6 @@
-a=input('Enter the string: ')
-if len(a)!=1:
-    b=a[::2]
+a = input('Enter the string: ')
+if len(a) != 1:
+    b = a[::2]
     print(b)
 else:
     print ('There is 1 character in the string')
