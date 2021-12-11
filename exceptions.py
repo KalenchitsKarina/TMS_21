c9323d18b21dc1f1c4100ef721b679f76242bf5b
KalenchitsKarina <kkalenchits@yandex.by>
2021-12-11 23:26:23 +0300

    test

--- exceptions.py
+++
@@ -1,9 +0,0 @@
-
-class WrongSignException(Exception):
-    def __init__(self, message = 'invalid sign'):
-        super().__init__(message)
-    def __str__(self):
-        return '{} is invalid input, can only accept ' \
-               '+, -, *, /, 0  as its values'.format(self.value)
-
- 
--- main.py
+++
@@ -1,7 +0,0 @@
-from ui_func import run
-
-def main():
-    run()
-
-if __name__ == '__main__':
-    main ()
